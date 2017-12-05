aa06dc91e6e199716ac1866245b4b7fc15600eb2
zhengkun1 <398786764@qq.com>
2017-12-05 19:32:24 +0800

    update

--- reactrain/src/router.js
+++ reactrain/src/router.js
@@ -1,14 +1,14 @@
 import React from 'react';
 import { Router, Route, Switch } from 'dva/router';
 import Basiclayout from './layouts/Basiclayout';
-import Userlayout from './layouts';
+// import Userlayout from './layouts';
 
 function RouterConfig({ history }) {
   return (
     <Router history={history}>
       <Switch>
         <Route path="/" exact component={Basiclayout} />
-        <Route path="/userinfo" component={Userlayout} />
+        {/* <Route path="/user" component={Userlayout} /> */}
       </Switch>
     </Router>
   );
